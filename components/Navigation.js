83abe3cd8036ecb42068a7a95d841c908a306764
ellaTerselic <terseler@mail.uc.edu>
2024-04-03 10:12:10 -0400

    navigation page created

---
+++ components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div>
+            <ul>
+                <li>Home Page</li>
+                <li>Rock Paper Scissors</li>
+                <li>Tic Tac Toe</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navigation;
